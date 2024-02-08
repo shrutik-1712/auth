f0d4ab6f6994ee2a7b0ef95a1e5d1ad3236d78f1
Shrutik Dawane <53071103+shrutik-1712@users.noreply.github.com>
2024-02-08 09:20:54 +0530

    Update main.jsx
    added axios in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+axios.defaults.baseURL="http://localhost:3000";
+axios.defaults.withCredentials=true;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
