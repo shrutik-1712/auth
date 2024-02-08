2a68ac8c7b0968590cd02a2ced821cd9074e75ed
Shrutik Dawane <shrutikdawane@gmail.com>
2024-02-08 08:55:10 +0530

    auth frontend

---
+++ src/components/LoginFom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginFom() {
+  return (
+    <div>LoginFom</div>
+  )
+}
+
+export default LoginFom
---
+++ src/pages/loginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginFom from '../components/LoginFom'
+
+export const loginForm = () => {
+  return (
+    <>
+    
+    <LoginFom/>
+
+    </>
+  )
+}
